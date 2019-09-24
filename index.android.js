ef78abcb3eb877cdb3ff3a17f6381a28f64f9793
superman <1501814877@qq.com>
2019-09-25 07:44:27 +0800

    first commit

---
+++ index.android.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Navigator
+} from 'react-native';
+import LaunchImage from './Component/Main/XMGLaunchImage'
+/**-------导入外部的组件类---------**/
+class XMGBuy extends Component {
+    render() {
+        return (
+            <Navigator
+                //启动时加载的路由
+                initialRoute={{name:'启动页',component:LaunchImage}}
+                //页面之间跳转时候的动画,更多动画看: node_modules/react-native/Libraries/CustomComponents/Navigator/NavigatorSceneConfigs.js
+                configureScene={()=>{
+                             return Navigator.SceneConfigs.PushFromRight;
+                        }}
+                // 两个参数就是initialRoute方法中传入的两个变量。这样在LaunchImage中就拿到了navigator用来跳转下一页面
+                // console.warn(JSON.stringify(route));
+                renderScene={(route,navigator)=>{
+                           let Component = route.component;
+                           return <Component {...route.passProps} navigator={navigator}/>;
+                        }}
+            />
+        );
+    }
+}
+
+AppRegistry.registerComponent('XMGBuy', () => XMGBuy);
